64785b595ca9dc1b9fd379ea9b310ab22fcf09f0
slide <mike@slide.se>
2013-10-21 22:08:46 +0200

    Removed dummy fragment.

--- src/se/slide/sgu/MainPodcastFragment.java
+++ src/se/slide/sgu/MainPodcastFragment.java
@@ -123,31 +123,4 @@ public class MainPodcastFragment extends Fragment implements ActionBar.OnNavigat
         }
     }
     
-    /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            /*
-            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
-            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-            */
-            
-            View view = inflater.inflate(R.layout.list_item_card_nocontent, null);
-            return view;
-        }
-    }
 }
