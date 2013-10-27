efc42f88f5c9b5f63255a6f90941d0b7cf2884bd
slide <mike@slide.se>
2013-10-27 13:14:24 +0100

    The GlobalContext need to be initialized before use.

--- src/se/slide/sgu/BootReceiver.java
+++ src/se/slide/sgu/BootReceiver.java
@@ -9,6 +9,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         
+        GlobalContext.INSTANCE.init(context);
         GlobalContext.INSTANCE.setScheduling();
         
     }
